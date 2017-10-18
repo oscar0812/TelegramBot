93f3091c6c74984c16763868bff9423262b86932
oscar0812 <oscar0812torres@gmail.com>
2017-10-18 12:46:27 -0500

    init commit

---
+++ src/MainClass.java
@@ -0,0 +1,20 @@
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.TelegramBotsApi;
+import org.telegram.telegrambots.exceptions.TelegramApiException;
+
+public class MainClass {
+    public static void main(String[] args) {
+        // Initialize Api Context
+        ApiContextInitializer.init();
+
+        // Instantiate Telegram Bots API
+        TelegramBotsApi botsApi = new TelegramBotsApi();
+
+        // Register our bot
+        try {
+            botsApi.registerBot(new BittleBot());
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+}
