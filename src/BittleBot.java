ae54dbdd6e0da5e4975bcab4410febd68f25c086
oscar0812 <oscar0812torres@gmail.com>
2017-10-18 13:14:09 -0500

    urban dictionary api

--- src/BittleBot.java
+++ src/BittleBot.java
@@ -1,9 +1,11 @@
+import com.bittle.urban.UrbanDictionary;
 import org.telegram.telegrambots.api.methods.send.SendMessage;
 import org.telegram.telegrambots.api.objects.Update;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.exceptions.TelegramApiException;
 
 public class BittleBot extends TelegramLongPollingBot {
+    UrbanDictionary dictionary = new UrbanDictionary();
     @Override
     public void onUpdateReceived(Update update) {
         // We check if the update has a message and the message has text
@@ -12,27 +14,41 @@ public class BittleBot extends TelegramLongPollingBot {
             String message_text = update.getMessage().getText();
             long chat_id = update.getMessage().getChatId();
 
-            SendMessage message = new SendMessage() // Create a message object object
-                    .setChatId(chat_id)
-                    .setText(message_text);
-            try {
-                execute(message); // Sending our message object to user
-            } catch (TelegramApiException e) {
-                e.printStackTrace();
+            if(message_text.startsWith("/ud ")){
+                String lookup = message_text.substring(message_text.indexOf(" ")).trim();
+                if(!lookup.isEmpty()){
+                    if(dictionary.searchWord(lookup)){
+                        sendTxtMsg(chat_id, dictionary.getRandomDefinition().getDefinition());
+                    }
+                }
+                else{
+                    sendTxtMsg(chat_id, "No definition found");
+                }
             }
         }
 
     }
 
+    public void sendTxtMsg(long chat_id, String msg){
+        SendMessage message = new SendMessage() // Create a message object object
+                .setChatId(chat_id)
+                .setText(msg);
+        try {
+            execute(message); // Sending our message object to user
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+
     @Override
     public String getBotUsername() {
         // TODO
-        return "bittle_bot";
+        return Constants.bot_username;
     }
 
     @Override
     public String getBotToken() {
         // TODO
-        return "419911698:AAG-6x2E2OrRfexMX3qtvFn-iAZcWrqR_c0";
+        return Constants.bot_token;
     }
 }
