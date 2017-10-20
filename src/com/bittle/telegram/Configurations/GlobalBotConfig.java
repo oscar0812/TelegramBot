548f20556a711b0627573e803d73803ec11f2d6c
oscar0812 <oscar0812torres@gmail.com>
2017-10-19 22:51:15 -0500

    fixed spacing

--- src/com/bittle/telegram/Configurations/GlobalBotConfig.java
+++ src/com/bittle/telegram/Configurations/GlobalBotConfig.java
@@ -24,17 +24,17 @@ public class GlobalBotConfig {
     private final String ROOT_PATH =
             MainClass.directory.global_dir();
 
-    private final String PATH_TO_ADMIN_XML= ROOT_PATH+ File.separator+"Admins.xml";
+    private final String PATH_TO_ADMIN_XML = ROOT_PATH + File.separator + "Admins.xml";
 
-    public GlobalBotConfig(){
+    public GlobalBotConfig() {
         File admin_file = new File(PATH_TO_ADMIN_XML);
 
-        if(!admin_file.exists()){
+        if (!admin_file.exists()) {
             setInitialAdmins();
         }
     }
 
-    private void setInitialAdmins(){
+    private void setInitialAdmins() {
         try {
 
             DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
@@ -54,17 +54,14 @@ public class GlobalBotConfig {
             transformer.transform(source, result);
 
             addNewAdmin(277439737, "OGBittle");
-            addNewAdmin(428043035, "Lexilovestacos");
-            addNewAdmin(250014386, "Disproven");
-            addNewAdmin(130107302, "lntegrity");
 
         } catch (ParserConfigurationException | TransformerException pce) {
             pce.printStackTrace();
         }
     }
 
-    public void addNewAdmin(long id, String username){
-        if(!adminExists(id)) {
+    public void addNewAdmin(long id, String username) {
+        if (!adminExists(id)) {
             try {
                 DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
                 DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
@@ -92,15 +89,15 @@ public class GlobalBotConfig {
         }
     }
 
-    public void removeAdmin(String username){
+    public void removeAdmin(String username) {
         removeAdmin("username", username);
     }
 
-    public void removeAdmin(long chat_id){
-        removeAdmin("id", chat_id+"");
+    public void removeAdmin(long chat_id) {
+        removeAdmin("id", chat_id + "");
     }
 
-    private void removeAdmin(String remove_by_this, String value_of_tag){
+    private void removeAdmin(String remove_by_this, String value_of_tag) {
         try {
             DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
             DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
@@ -115,7 +112,7 @@ public class GlobalBotConfig {
                 Node current_user = users.item(x);
                 String val = current_user.getAttributes().getNamedItem(remove_by_this).getTextContent();
 
-                if(val.equals(value_of_tag)){
+                if (val.equals(value_of_tag)) {
                     // found user to remove
                     root.removeChild(current_user);
                 }
@@ -136,14 +133,14 @@ public class GlobalBotConfig {
     }
 
     public boolean adminExists(long user_id) {
-        return adminExists("id", user_id+"");
+        return adminExists("id", user_id + "");
     }
 
-    public boolean adminExists(String username){
+    public boolean adminExists(String username) {
         return adminExists("username", username);
     }
 
-    private boolean adminExists(String find_by_this, String value_of_tag){
+    private boolean adminExists(String find_by_this, String value_of_tag) {
         try {
 
             DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
@@ -161,7 +158,7 @@ public class GlobalBotConfig {
                 Node current_user = users.item(x);
                 String current_id = current_user.getAttributes().getNamedItem(find_by_this).getTextContent();
 
-                if(current_id.equals(value_of_tag)){
+                if (current_id.equals(value_of_tag)) {
                     player_exists = true;
                     break;
                 }
@@ -187,13 +184,15 @@ public class GlobalBotConfig {
             //loop through users
             NodeList users = root.getChildNodes();
 
-            String all = "";
+            StringBuilder all = new StringBuilder();
             for (int x = 0; x < users.getLength(); x++) {
                 Node current_user = users.item(x);
                 String current_u = current_user.getAttributes().getNamedItem("username").getTextContent();
-                all+=("@"+current_u+"\n");
+                all.append("@");
+                all.append(current_u);
+                all.append("\n");
             }
-            return all+"\n";
+            return all.toString() + "\n";
 
         } catch (Exception e) {
             e.printStackTrace();
