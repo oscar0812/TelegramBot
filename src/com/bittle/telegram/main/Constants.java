0dd5ebc4e2ed68f827b7c07e1bc7ac49faa8a17d
oscar0812 <oscar0812torres@gmail.com>
2017-11-01 14:20:27 -0500

    for use

--- src/com/bittle/telegram/main/Constants.java
+++ src/com/bittle/telegram/main/Constants.java
@@ -2,7 +2,7 @@ package com.bittle.telegram.main;
 
 public class Constants {
     public static final int MAX_MESSAGE_INT = 4085;
-    public static final String BOT_USERNAME = "bittle_bot";
-    public static final String BOT_TOKEN = "419911698:AAEDjjM2ibWjxunn073khxL0N1LlEWYbA4A";
+    public static final String BOT_USERNAME = "bot_username";
+    public static final String BOT_TOKEN = "bot_token";
 
 }
